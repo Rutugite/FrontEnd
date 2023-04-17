df54ec2835b2c97ef5a7338a5a7d5cbbe2b2b9ca
rutujagite307@gmail.com <Rutugite>
2023-04-17 15:10:37 +0530

    first commit

---
+++ daythree/daythree.js
@@ -0,0 +1,50 @@
+//singleline comment
+/*Multiline comment
+alert("Congratulation!")
+alert("Hello")
+alert(4+2)
+let num=15;
+    alert(num)
+var no=10;
+    alert(no)
+const a=167;
+    alert(a);
+a=122;
+    alert(a);
+*/
+//Assignment operator
+/*let a=13;
+    a*=5;
+alert(a)
+
+var n=5;
+    n+=9;
+alert(n)*/
+
+/*relational operator
+var res=12=0;
+    alert(res)*/
+
+/*logical operator
+var x=13;
+var y=12;
+    alert(x==y && x>y)
+
+if(x>y)
+    alert("Rutuja")
+else
+    alert("Gite")
+alert(typeof("Rutuja"));
+
+let age=prompt("Whats your age?",100)
+    alert("Your age is "+age)*/
+
+let img = document.createElement("img");
+img.src = "https://picsum.photos/200/301";
+    
+img.style.border = "10px solid black";
+img.style.borderRadius = "10px";
+    
+document.body.appendChild(img);
+
+
